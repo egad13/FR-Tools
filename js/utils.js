7a443c739992b30e4573624a4b3a4e56cd8d3484
egad13 <egad.coding@gmail.com>
2023-07-30 14:46:31 -0400

    Update event utils
    New function for triggering events. addEvt() is now just shorthand for addEventListener, because I realised it's really well supported and the old function was doing way too much work.

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 
 /** A number of generally useful functions. */
 var Util = Util || (function(){
-	
+
 	// Some aliases/shorthands for basic dom searching/manipulation
 
 	/** Gets a dom element using its id. */
@@ -26,65 +26,43 @@ var Util = Util || (function(){
 	function createElt(tag, props) {
 		return Object.assign(document.createElement(tag), props);
 	}
-	
+
 	// Other miscellaneous useful functions
-	
+
 	/** Like Array.map(), but for object literals; ie, performs the given function on all its properties. If the given object has child object literals, it will recursively apply the function to the children objects' properties too.
 	 * @param {object} obj The object whose properties to perform the function on.
 	 * @param {function} func The function to apply to the properties.*/
 	// TODO the check to see if a value is itself an object literal returns true for functions. Figure out if there's a brief way to make it not do that.
-	function mapObj(obj, func){			
+	function mapObj(obj, func){
 		return Object.fromEntries(
-			Object.entries(obj).map(([k, v]) => 
+			Object.entries(obj).map(([k, v]) =>
 				[k, (Object.is(v.constructor, Object)) ? mapObj(v, func) : func(v)]
 			)
 		);
 	}
 
-	/** A cross-browser compatible method to assign an event to an element.
-	* Supports all standard events. */
-	function addEvt(elem, type, handler){
-		if (supportsPassive == undefined){
-			var supportsPassive = false;
-			try {
-				var opts = Object.defineProperty({}, 'passive', {
-					get: function() {
-						supportsPassive = true;
-					}
-				});
-				window.addEventListener("testPassive", null, opts);
-				window.removeEventListener("testPassive", null, opts);
-			} catch (e) {}
-		}
-		
-		var ontype = "on" + type;
-		
-		if(elem.addEventListener){
-			elem.addEventListener(type, handler, ((type == 'touchstart' || type == 'mousewheel') && supportsPassive ? { passive: true } : false));
-		}
-		else if(elem.attachEvent)
-			elem.attachEvent(ontype, handler);
-		else {
-			if (elem[ontype]) {
-				var prevHandler = elem[ontype];
-				elem[ontype] = function() { prevHandler(); handler(); };
-			}
-			else
-				elem[ontype] = handler;
-		}
+	/** Trigger a standard event on an element. */
+	function triggerEvt(elt, evtName){
+		const evt = new Event(evtName, { view: window, bubbles: true, cancelable: true });
+		elt.dispatchEvent(evt);
 	}
-	
+
+	/** Assign a standard event handler to an element. */
+	function addEvt(elem, type, handler){
+		elem.addEventListener(type, handler, ((type == 'touchstart' || type == 'mousewheel') ? { passive: true } : false));
+	}
+
 	/** Determines whether text displayed on the given (hex) background
 	* colour should be white or black in order to be easiest to read. */
 	function textColourForBg(color) {
 		var r, g, b, hsp;
-		
+
 		// Convert to RGB
 		color = +("0x" + color);
 		r = color >> 16;
 		g = color >> 8 & 255;
 		b = color & 255;
-		
+
 		// HSP equation from http://alienryderflex.com/hsp.html
 		hsp = Math.sqrt(
 			0.299 * (r * r) +
@@ -99,9 +77,9 @@ var Util = Util || (function(){
 		}
 		return "#fff";
 	}
-	
+
 	return {
-		
+
 		get: get,
 		getClz: getClz,
 		query: query,
@@ -109,11 +87,10 @@ var Util = Util || (function(){
 		chosenOp: chosenOp,
 		createElt: createElt,
 		mapObj: mapObj,
+		triggerEvt: triggerEvt,
 		addEvt: addEvt,
 		textColourForBg: textColourForBg
-		
+
 	};
-	
+
 }());
-
-
